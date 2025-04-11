b5c44fe5c2f3bf2a2133a48917d745fcdaf4d31c
Sonu64 <sonusantu64@gmail.com>
2025-04-11 08:00:30 +0530

    Added Movement.cs, used InputAction class to Serialize a field called thrust and checked if a keybinding was pressed.

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.InputSystem; // Input System namespace
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] InputAction thrust;
+
+    /**
+     OnEnable() is called every time a script or GameObject is enabled. 
+     This is important because you often want input actions to be re-enabled
+     if the object is reactivated at runtime (e.g., switching UI screens, 
+     toggling objects on/off).
+    */
+    private void OnEnable() {
+        thrust.Enable();
+    }
+
+    private void Update() {
+        if (thrust.IsPressed()) {
+            Debug.Log("You Pressed Thrust key !");
+        }
+    }
+}
