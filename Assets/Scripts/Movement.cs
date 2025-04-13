3cf2823645f798d1193769c0e80bb4716564ce67
Sonu64 <sonusantu64@gmail.com>
2025-04-13 07:41:41 +0530

    Added Rotation.
    Added rotationSpeed and also created Methods to make code cleaner and reusable with parameters.

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,7 +4,10 @@ using UnityEngine.InputSystem; // Input System namespace
 public class Movement : MonoBehaviour
 {
     [SerializeField] InputAction thrust;
+    [SerializeField] InputAction rotation;
     [SerializeField] float thrustStrength = 100f;
+    [SerializeField] float rotationStrength = 60f;
+
     Rigidbody rigidbody;
 
     /**
@@ -16,6 +19,7 @@ public class Movement : MonoBehaviour
 
     private void OnEnable() {
         thrust.Enable();
+        rotation.Enable();
     }
 
     private void Start() {
@@ -23,10 +27,34 @@ public class Movement : MonoBehaviour
     }
 
     private void FixedUpdate() {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    private void ProcessThrust() {
         if (thrust.IsPressed()) {
             // Vector3.up is a Vector3 = (0,1,0), Give very high thrustStrength
             // as fixedDeltaTime is much lesser that DeltaTime
-            rigidbody.AddRelativeForce(Vector3.up*Time.fixedDeltaTime*thrustStrength);
+            rigidbody.AddRelativeForce(Vector3.up * Time.fixedDeltaTime * thrustStrength);
         }
+    }
+
+    private void ProcessRotation() {
+        float rotationInput = (rotation.ReadValue<float>());
+        
+        // if-elseIf is better to ensure mutual Exclusiveness such that both doesn't happen
+        // at the same time at any condition. Shouldn't happen though.
+
+        if (rotationInput < 0) { // -1 is for Right Rotation ABOUT THE Z-AXIS
+            ApplyRotation(rotationStrength);
+            //transforms to Vector3.forward * rotationStrength * Time.fixedDeltaTime for (0,0,1)
+        } else if (rotationInput > 0) { // +1 is for Left Rotation ABOUT THE Z-AXIS
+            ApplyRotation(-rotationStrength);
+            //transforms to Vector3.forward * (-rotationStrength) * Time.fixedDeltaTime for (0,0,-1)
+        }
+    }
+
+    private void ApplyRotation(float currentFrameRotation) {
+        transform.Rotate(Vector3.forward * currentFrameRotation * Time.fixedDeltaTime);
     }
 }
