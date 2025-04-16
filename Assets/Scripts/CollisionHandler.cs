afe3322b323d1f4421973e52ca0a17385c6cf3c3
Sonu64 <sonusantu64@gmail.com>
2025-04-16 07:35:08 +0530

    Added CollisionHandler.cs
    Earlier by mistake all collision logic was in the movement.cs file.

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,16 +1,28 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+    private void OnCollisionEnter(Collision collision) {
+        switch (collision.gameObject.tag) {
+            case "Fuel":
+                Debug.Log("Fuel Hit");
+                break;
+            case "Friendly":
+                Debug.Log("In Launchpad");
+                break;
+            case "Finish":
+                Debug.Log("Reached Landing Pad");
+                break;
+            default:
+                ReloadLevel();
+                break;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void ReloadLevel() {
+        int sceneIndex = SceneManager.GetActiveScene().buildIndex; // will be dynamic for other scenes as well
+        SceneManager.LoadScene(sceneIndex);
     }
 }
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -35,22 +35,7 @@ public class Movement : MonoBehaviour
         ProcessRotation();
     }
 
-    private void OnCollisionEnter(Collision collision) {
-        switch(collision.gameObject.tag) {
-            case "Fuel":
-                Debug.Log("Fuel Hit");
-                break;
-            case "Friendly":
-                Debug.Log("In Launchpad");
-                break;
-            case "Finish":
-                Debug.Log("Reached Landing Pad");
-                break;
-            default:
-                ReloadLevel();
-                break;
-        }
-    }
+
 
     private void ProcessThrust() {
         if (thrust.IsPressed()) {
@@ -87,8 +72,4 @@ public class Movement : MonoBehaviour
         rigidBody.freezeRotation = false;
     }
 
-    private void ReloadLevel() {
-        int sceneIndex = SceneManager.GetActiveScene().buildIndex; // will be dynamic for other scenes as well
-        SceneManager.LoadScene(sceneIndex);
-    }
 }
