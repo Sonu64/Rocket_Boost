7663cb1c794ef1b075136eea411a3ff0c86bd190
Sonu64 <sonusantu64@gmail.com>
2025-04-17 14:08:56 +0530

    added delay using invoke

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,28 +1,59 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class CollisionHandler : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+public class CollisionHandler : MonoBehaviour {
+
+    [SerializeField] float delay = 200f;
+
+
+    /* 
+     * 
+     * 
+     * The Invoke() and InvokeRepeating() methods in Unity cannot call static 
+       methods due to how they work internally:
+     * Reflection-based implementation - These methods use reflection to find and 
+       call methods by name at runtime.
+     * Instance binding requirement - Invoke is a MonoBehaviour method that looks for 
+       methods within the specific instance that called it. It needs an object instance 
+       to search within.
+
+      * Method signature restrictions - Invoke can only call methods that:
+        --> Take zero parameters
+        --> Are instance methods (not static)
+        --> Are part of the calling MonoBehaviour
+      * 
+      * 
+      */
+
+
     private void OnCollisionEnter(Collision collision) {
         switch (collision.gameObject.tag) {
-            case "Fuel":
-                Debug.Log("Fuel Hit");
-                break;
             case "Friendly":
                 Debug.Log("In Launchpad");
                 break;
             case "Finish":
                 Debug.Log("Reached Landing Pad");
-                LoadNextLevel();
+                StartVictorySequence();
                 break;
             default:
-                ReloadLevel();
+                StartCrashSequence();
                 break;
         }
     }
 
-    private static void LoadNextLevel() {
+    private void StartCrashSequence() {
+        // Disabling Controls once we crash
+        GetComponent<Movement>().enabled = false;
+        Invoke("ReloadLevel", delay);
+    }
+
+    private void StartVictorySequence() {
+        // Disabling Controls once we crash
+        GetComponent<Movement>().enabled = false;
+        Invoke("LoadNextLevel", delay);
+    }
+
+    private void LoadNextLevel() {
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         int nextSceneIndex = currentSceneIndex + 1;
 
